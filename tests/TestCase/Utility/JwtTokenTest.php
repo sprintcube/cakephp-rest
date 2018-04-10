1c17a0ea289c8a888d16bc28f37949d5c13590ac
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-10 20:13:41 +0530

    Add unit tests

---
+++ tests/TestCase/Utility/JwtTokenTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Rest\Test\TestCase\Utility;
+
+use Cake\TestSuite\TestCase;
+use Rest\Utility\JwtToken;
+
+class JwtTokenTest extends TestCase {
+
+    /**
+     * test generate function
+     */
+    public function testGenerate() {
+        $payload = [
+            'id' => 1,
+            'email' => 'john@example.com'
+        ];
+
+        $this->assertNotEmpty(JwtToken::generate($payload));
+        $this->assertEquals(false, JwtToken::generate());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,9 @@
 /**
  * Test suite bootstrap for Rest.
  *
+ */
+
+/**
  * This function is used to find the location of CakePHP whether CakePHP
  * has been installed as a dependency of the plugin, or the plugin is itself
  * installed as a dependency of an application.
@@ -20,11 +23,73 @@ $findRoot = function ($root) {
 $root = $findRoot(__FILE__);
 unset($findRoot);
 
-chdir($root);
-
-if (file_exists($root . '/config/bootstrap.php')) {
-    require $root . '/config/bootstrap.php';
-
-    return;
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
 }
-require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
+
+define('ROOT', $root);
+define('APP_DIR', 'App');
+define('WEBROOT_DIR', 'webroot');
+define('APP', ROOT . '/tests/App/');
+define('CONFIG', ROOT . '/tests/config/');
+define('WWW_ROOT', ROOT . DS . WEBROOT_DIR . DS);
+define('TESTS', ROOT . DS . 'tests' . DS);
+define('TMP', ROOT . DS . 'tmp' . DS);
+define('LOGS', TMP . 'logs' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+
+require ROOT . '/vendor/autoload.php';
+require CORE_PATH . 'config/bootstrap.php';
+
+Cake\Core\Configure::write('App', ['namespace' => 'Rest\Test\App']);
+
+Cake\Core\Configure::write('debug', true);
+
+$TMP = new \Cake\Filesystem\Folder(TMP);
+$TMP->create(TMP . 'cache/models', 0777);
+$TMP->create(TMP . 'cache/persistent', 0777);
+$TMP->create(TMP . 'cache/views', 0777);
+
+$cache = [
+    'default' => [
+        'engine' => 'File'
+    ],
+    '_cake_core_' => [
+        'className' => 'File',
+        'prefix' => 'rest_myapp_cake_core_',
+        'path' => CACHE . 'persistent/',
+        'serialize' => true,
+        'duration' => '+10 seconds'
+    ],
+    '_cake_model_' => [
+        'className' => 'File',
+        'prefix' => 'rest_my_app_cake_model_',
+        'path' => CACHE . 'models/',
+        'serialize' => 'File',
+        'duration' => '+10 seconds'
+    ]
+];
+
+Cake\Cache\Cache::config($cache);
+
+Cake\Core\Configure::write('Session', [
+    'defaults' => 'php'
+]);
+
+Cake\Core\Plugin::load('Rest', ['path' => ROOT . DS, 'autoload' => true, 'bootstrap' => true]);
+
+Cake\Routing\DispatcherFactory::add('Routing');
+Cake\Routing\DispatcherFactory::add('ControllerFactory');
+
+// Ensure default test connection is defined
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite:///:memory:');
+}
+
+Cake\Datasource\ConnectionManager::config('test', [
+    'url' => getenv('db_dsn'),
+    'timezone' => 'UTC'
+]);
