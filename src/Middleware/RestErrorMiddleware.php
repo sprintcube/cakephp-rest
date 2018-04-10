ff3cd09e14da9c8e756812c8cd184b93a936956f
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-10 16:55:13 +0530

    Fix phpcs

--- src/Middleware/RestErrorMiddleware.php
+++ src/Middleware/RestErrorMiddleware.php
@@ -30,11 +30,9 @@ class RestErrorMiddleware extends ErrorHandlerMiddleware
 
                 return $next($request, $response);
             } catch (Exception $e) {
-
                 return $this->handleException($e, $request, $response);
             }
         } else {
-
             return $next($request, $response);
         }
     }
